938c9a20a0c59c6c8543ffb76e284abdd489a325
Comf2 <otischaka4@gmail.com>
2023-02-08 09:48:33 -0500

    updated nav and added cookies

--- production/js/main.js
+++ production/js/main.js
@@ -1,15 +1,19 @@
-const housewareURL = "https://acnhapi.com/v1a/houseware/";
+//TODO:
+//add searchbar to nav
+//if signed in edit the mobile nav
+//fix the modal crop styles for mobile
+const housewareURL = 'https://acnhapi.com/v1a/houseware/';
 
 //-----mobile navbar toggle-----//
-const toggleNavButton = document.querySelector(".toggle-nav-button");
-const navBar = document.querySelector(".nav-bar");
-const navList = document.querySelector(".nav-list");
+const toggleNavButton = document.querySelector('.toggle-nav-button');
+const navBar = document.querySelector('.nav-bar');
+const navList = document.querySelector('.nav-list');
 let navOpen = false;
 
 const desktopBreakPoint = 900;
 
 //when button is clicked toggle the nav bar
-toggleNavButton.addEventListener("click", (e) => {
+toggleNavButton.addEventListener('click', (e) => {
   toggleNav();
 });
 
@@ -17,13 +21,13 @@ toggleNavButton.addEventListener("click", (e) => {
 const toggleNav = () => {
   navOpen = !navOpen;
   if (navOpen) {
-    toggleNavButton.setAttribute("data-state", "opened");
-    toggleNavButton.setAttribute("aria-expanded", "true");
+    toggleNavButton.setAttribute('data-state', 'opened');
+    toggleNavButton.setAttribute('aria-expanded', 'true');
 
     openNav();
   } else if (!navOpen) {
-    toggleNavButton.setAttribute("data-state", "closed");
-    toggleNavButton.setAttribute("aria-expanded", "false");
+    toggleNavButton.setAttribute('data-state', 'closed');
+    toggleNavButton.setAttribute('aria-expanded', 'false');
 
     closeNav();
   }
@@ -31,26 +35,26 @@ const toggleNav = () => {
 
 //styles nav bar to be open when its toggled
 const openNav = () => {
-  navBar.style.width = "75vw";
-  navList.style.display = "grid";
+  navBar.style.width = '75vw';
+  navList.style.display = 'grid';
   setTimeout(() => {
-    navList.style.opacity = "1";
+    navList.style.opacity = '1';
   }, 500);
 };
 
 //styles nav bar to be closed when its toggled
 const closeNav = () => {
-  navBar.style.width = "0";
-  navList.style.opacity = "0";
+  navBar.style.width = '0';
+  navList.style.opacity = '0';
   setTimeout(() => {
-    navList.style.display = "none";
+    navList.style.display = 'none';
   }, 300);
 };
 
-window.addEventListener("resize", function (e) {
+window.addEventListener('resize', function (e) {
   if (window.innerWidth >= desktopBreakPoint) {
-    navBar.style = "";
-    navList.style = "";
+    navBar.style = '';
+    navList.style = '';
   } else if (window.innerWidth <= desktopBreakPoint) {
     navOpen = true;
     toggleNav();
@@ -58,8 +62,8 @@ window.addEventListener("resize", function (e) {
 });
 //---account Dropdown for navabr---//
 
-const accountIcon = document.querySelector("#account-icon");
-const accountDropdown = document.querySelector(".account-dropdown");
+const accountIcon = document.querySelector('#account-icon');
+const accountDropdown = document.querySelector('.account-dropdown');
 
 let dropdownOpen = false;
 
@@ -72,79 +76,88 @@ const toggleDropdown = () => {
 };
 
 const openDropdown = () => {
-  accountDropdown.setAttribute("data-open", "true");
-  accountDropdown.style.display = "block";
+  accountDropdown.setAttribute('data-open', 'true');
+  accountDropdown.style.display = 'block';
 };
 const closeDropdown = () => {
-  accountDropdown.setAttribute("data-open", "false");
+  accountDropdown.setAttribute('data-open', 'false');
   setTimeout(() => {
-    accountDropdown.style.display = "none";
+    accountDropdown.style.display = 'none';
   }, 300);
 };
 
 //dark and light mode
-const lightModeToggleButton = document.querySelectorAll(".toggle-light-mode");
+const lightModeToggleButton = document.querySelectorAll('.toggle-light-mode');
 
 //color schemes
 
 //dark
-const mainDark = "#121212";
-const secondaryDark = "#1f1f1f";
-const mainDarkText = "#dadada";
-const secondaryDarkText = "#bdbdbd";
+const mainDark = '#121212';
+const secondaryDark = '#1f1f1f';
+const mainDarkText = '#dadada';
+const secondaryDarkText = '#bdbdbd';
 
 //light
-const mainLight = "#FFFFFA";
-const secondaryLight = "#ECECEC";
-const mainLightText = "#313131";
-const secondaryLightText = "#bdbdbd";
+const mainLight = '#FFFFFA';
+const secondaryLight = '#ECECEC';
+const mainLightText = '#313131';
+const secondaryLightText = '#bdbdbd';
 
-const root = document.querySelector(":root");
+const root = document.querySelector(':root');
 
 lightModeToggleButton.forEach(function (button) {
   button.onclick = () => toggleLightMode();
 });
 
 const setLightMode = (darkmode) => {
-  const darkLogos = document.querySelectorAll(".logo-dark-switch");
+  const darkLogos = document.querySelectorAll('.logo-dark-switch');
 
   darkLogos.forEach((logo) => {
-    logo.src = "../svg/nook-bay-light.svg";
+    logo.src = '../svg/nook-bay-light.svg';
   });
   if (darkmode) {
-    root.style.setProperty("--main-bg", mainDark);
-    root.style.setProperty("--secondary-bg", secondaryDark);
-    root.style.setProperty("--main-text", mainDarkText);
-    root.style.setProperty("--secondary-text", secondaryDarkText);
+    root.style.setProperty('--main-bg', mainDark);
+    root.style.setProperty('--secondary-bg', secondaryDark);
+    root.style.setProperty('--main-text', mainDarkText);
+    root.style.setProperty('--secondary-text', secondaryDarkText);
 
     darkLogos.forEach((logo) => {
-      logo.src = "../svg/nook-bay-light.svg";
+      logo.src = '../svg/nook-bay-light.svg';
     });
 
-    window.localStorage.setItem("darkMode", true);
+    window.localStorage.setItem('darkMode', true);
   } else if (!darkmode) {
-    root.style.setProperty("--main-bg", mainLight);
-    root.style.setProperty("--secondary-bg", secondaryLight);
-    root.style.setProperty("--main-text", mainLightText);
-    root.style.setProperty("--secondary-text", secondaryLightText);
+    root.style.setProperty('--main-bg', mainLight);
+    root.style.setProperty('--secondary-bg', secondaryLight);
+    root.style.setProperty('--main-text', mainLightText);
+    root.style.setProperty('--secondary-text', secondaryLightText);
 
     darkLogos.forEach((logo) => {
-      logo.src = "../svg/NookBay.svg";
+      logo.src = '../svg/NookBay.svg';
     });
 
-    window.localStorage.setItem("darkMode", false);
+    window.localStorage.setItem('darkMode', false);
   }
+  setDarkmodeCookie();
 };
 
 const toggleLightMode = () => {
-  let isDarkMode = window.localStorage.getItem("darkMode");
-  let darkMode = isDarkMode === "false";
+  let isDarkMode = window.localStorage.getItem('darkMode');
+  let darkMode = isDarkMode === 'false';
   setLightMode(darkMode);
 };
 
 const getDarkMode = () => {
-  let isDarkMode = window.localStorage.getItem("darkMode");
-  let darkMode = isDarkMode === "true";
+  let isDarkMode = document.cookie;
+  console.log(isDarkMode);
+  let darkMode = isDarkMode === 'true';
   setLightMode(darkMode);
 };
 getDarkMode();
+//---**--Saving Darkmode To Cookies ---**--//
+
+//cookies can only store darkmode
+function setDarkmodeCookie() {
+  let isDarkmode = localStorage.getItem('darkMode');
+  document.cookie = `${isDarkmode};`;
+}
